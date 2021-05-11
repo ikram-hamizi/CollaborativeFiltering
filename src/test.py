bc5dba9011e1a98103ec079216d7f34ad047515b
github.com\ikram-hamizi <hamizii@mymail.vcu.edu>
2021-05-12 02:48:42 +0300

    temp commit

--- src/test.py
+++ src/test.py
@@ -9,13 +9,24 @@ import scripts.dataExtract as dataExtract
 #import python libraries
 import pandas as pd
 import numpy as np
+import pickle
 
 
 userID = 330
 
 train, _ = dataExtract.get_data() # ['userId', 'movieId', 'rating'] (dataframes)
-
 ncf = RecommendMovies.NCF(train) # recomennder objects
+
+with open('P1.pkl','rb') as f:
+	P1 = pickle.load(f)
+with open('Q1.pkl','rb') as f:
+	Q1 = pickle.load(f)
+
+with open('P2.pkl','rb') as f:
+	P2 = pickle.load(f)
+with open('Q2.pkl','rb') as f:
+	Q2 = pickle.load(f)
+
 
 #*********************
 # Already liked movies
@@ -39,11 +50,11 @@ print("---✰" * 14)
 print(new_predicted_to_user)
 
 
-
+"""
 #=============
 #  MF w/ GD
 #=============
-_, new_predicted_to_user = predictMF(userID, P2, Q2, train)
+#_, new_predicted_to_user = predictMF(userID, P2, Q2, train)
 print("✰---" * 13)
 print(f"Top 5 movie recommendations for user {userID} -- MF (GD)")
 print("---✰" * 13)
@@ -59,4 +70,5 @@ print(new_predicted_to_user)
 print("✰---" * 12)
 print(f"Top 5 movie recommendations for user {userID} -- NN")
 print("---✰" * 12)
-#print(new_predicted_to_user)
+print(new_predicted_to_user)
+"""
