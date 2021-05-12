39ca857623b4bc39e41138b705ac6e9f5677f85b
github.com\ikram-hamizi <hamizii@mymail.vcu.edu>
2021-05-12 04:28:31 +0300

    temp commit for testing

--- src/test.py
+++ src/test.py
@@ -22,7 +22,7 @@ ncf = RecommendMovies.NCF(train) # recomennder objects
 #*********************
 # Already liked movies
 #*********************
-top_liked_by_user, new_predicted_to_user = predictMF(userID, P1, Q1, train)
+top_liked_by_user, new_predicted_to_user = ncf.predictMF(userID, P1, Q1, train)
 print("****" * 8)
 print(f"Top 5 liked movies by user {userID}")
 print("****" * 8)
@@ -55,7 +55,7 @@ with open('P2.pkl','rb') as f:
 with open('Q2.pkl','rb') as f:
 	Q2 = pickle.load(f)
 	
-#_, new_predicted_to_user = predictMF(userID, P2, Q2, train)
+#_, new_predicted_to_user = ncf.predictMF(userID, P2, Q2, train)
 print("✰---" * 13)
 print(f"Top 5 movie recommendations for user {userID} -- MF (GD)")
 print("---✰" * 13)
@@ -69,7 +69,7 @@ print(new_predicted_to_user)
 #=============
 model = torch.load("NCF.pth")
 
-#_, new_predicted_to_user = predictNN(userID, model, train)
+#_, new_predicted_to_user = ncf.predictNN(userID, model, train)
 print("✰---" * 12)
 print(f"Top 5 movie recommendations for user {userID} -- NN")
 print("---✰" * 12)
