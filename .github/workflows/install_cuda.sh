d904635a7f1d7a2e0d8d8fc1557486f1fd5f799f
github.com\ikram-hamizi <hamizii@mymail.vcu.edu>
2021-05-12 06:13:56 +0300

    test cuda

--- .github/workflows/install_cuda.sh
+++ .github/workflows/install_cuda.sh
@@ -1,12 +1,12 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin &&\
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600 &&\
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600 &&\
 wget <https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb> &&\
-dpkg -i cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb &&\
-apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub  &&\
-apt-get update  &&\
-apt-get install cuda &&\
+sudo dpkg -i cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb &&\
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub  &&\
+sudo apt-get update  &&\
+sudo apt-get install cuda &&\
 bitfusion run -n 1 nvidia-smi &&\
 cd /usr/local/cuda/samples/0_Simple/matrixMul  &&\
-make &&\
+sudo make &&\
 bitfusion run -n 1 ./matrixMul &&\
 
